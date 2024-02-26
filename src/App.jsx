ed34137a521487dc9c8b74839bb15c8a8bc46d55
official-MKV <vemmaks84@gmail.com>
2024-02-26 14:00:33 +0000

    layout set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="signin" element={<Signin />} />
+        <Route path="/dahsboard" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="departments" element={<Departments />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="goals" element={<Goals />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
