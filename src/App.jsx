089381bdc3e6d7d86630d9a2e4c7c6b286f9d201
official-MKV <vemmaks84@gmail.com>
2024-02-26 14:26:27 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Departments from "./pages/Departments";
+import Analytics from "./pages/Analytics";
+import Error from "./pages/Error";
+import Goals from "./pages/Goals";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="signin" element={<Signin />} />
-        <Route path="/dahsboard" element={<Layout />}>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="departments" element={<Departments />} />
           <Route path="analytics" element={<Analytics />} />
---
+++ src/components/Layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout = () => {
+  return <div className="text-black">Layout</div>;
+};
+
+export default Layout;
