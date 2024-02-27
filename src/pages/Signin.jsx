525145b9d6d9ce346b612fd2df8525e50d642a29
Ashley Olika <olikaashley@gmail.com>
2024-02-27 15:20:25 +0100

    added svg

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -10,7 +10,7 @@ const Signin = () => {
       <div className="w-full flex  items-center justify-cente  r lg:w-1/2" >
       <Form/>
       </div>
-      <div className="hidden relative lg:flex h-full items-center justify-center bg-gray-200">
+      <div className="relative flex h-full items-center justify-center bg-gray-200">
         <div className="w-[300px] h-[67px] top-[45px] left-[43px]" >
           <img src={Image} />
           <div className= "w-full absolute h-1/2 bg-white/10" />
