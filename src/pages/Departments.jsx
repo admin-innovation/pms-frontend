7988e430aa58d118a6c675098d6babc841494468
Ashley Olika <olikaashley@gmail.com>
2024-02-28 11:22:50 +0100

    Department Page Init

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Departments = () => {
-  return <div className="text-[blue] text-[100px]">Hello</div>;
+  return (
+    <div>
+      <p className=" text-black font-[600] text-[16px]">List Of Directorates/Departments</p>
+    </div>
+
+  );
+
+
 };
 
 export default Departments;
