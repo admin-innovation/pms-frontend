bfcfb5830a418290bb516c1c073745382cb183cc
Ashley Olika <olikaashley@gmail.com>
2024-02-28 15:31:24 +0100

    assets for department added

---
+++ src/assets/index.jsx
@@ -0,0 +1,33 @@
+import Facilities from './facilities.svg';
+import Marketing from './Marketing and Stakeholders Engagement.svg';
+import Network from './Network Operation.svg';
+import Innovation from './innovation.svg';
+import Finance from './finance.svg';
+import HumanR from './HR.svg';
+import Budgeting from './budgeting.svg';
+import InfoTS from './ITS.svg';
+import Administration from './admin.svg';
+import Broadcast from './broadcast.svg';
+import Sales from './sales.svg';
+import Business from './business office.svg';
+import SatNav from './Sattelite Nav.svg';
+import Broadband from './broadband.svg';
+import SatCtrl from './sattelite ctrl.svg';
+
+export {
+  Facilities,
+  Marketing,
+  Network,
+  Innovation,
+  Finance,
+  HumanR,
+  Budgeting,
+  InfoTS,
+  Administration,
+  Broadcast,
+  Sales,
+  Business,
+  SatNav,
+  Broadband,
+  SatCtrl
+};
--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import {Facilities, Marketing, Network, Innovation, Finance, HumanR, Budgeting, InfoTS, Administration, Broadcast, Sales, Business, SatNav, Broadband, SatCtrl} from "../assets/index";
 
 const Departments = () => {
   return (
+    <>
     <div>
-      <p className=" text-black font-[600] text-[16px]">List Of Directorates/Departments</p>
+      <p className=" text-black font-[600] text-[16px]">List Of Departments</p>
     </div>
+    <div>
+      <div>
+      </div>
+    </div>
+    </>
 
   );
 
