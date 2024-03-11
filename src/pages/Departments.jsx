47c355655dc99201db1431bff7aeaa98d105841c
Ashley Olika <olikaashley@gmail.com>
2024-03-11 10:49:40 +0100

    Department pages done AGAIN

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -5,36 +5,33 @@ const Departments = () => {
   console.log(Facilities)
   return (
     <>
-    <div>
-      <p className=" text-black font-[600] text-[16px]">List Of Departments</p>
+    <div className="w-[1320px] h-[50px] rounded-[8px] font-[700px] bg-[#BACCE7] flex flex-col justify-end">Directorate of Finance & Administration</div>
+    <div className="grid grid-cols-5 gap-x-2 gap-y-2">
+    <DepartmentTile title="Administration" image={Administration} link=""/>
+    <DepartmentTile title="Finance" image={Finance} link=""/>
+    <DepartmentTile title="Human Resources" image={HumanR} link=""/>
+    <DepartmentTile title="Budgeting and Planning" image={Budgeting} link=""/>
     </div>
-    <div>
 
-      <div className="grid grid-cols-5 gap-x-2 gap-y-2">
-        <DepartmentTile title="Facility" image={Facilities} link=""/>
-        <DepartmentTile title="Marketing and Stakeholders Engagement" image={Marketing} link=""/>
-        <DepartmentTile title="Network Operation Center" image={Network} link=""/>
-        <DepartmentTile title="Innovation" image={Innovation} link=""/>
-        <DepartmentTile title="Finance" image={Finance} link=""/>
-      
+    <div className="w-[1320px] h-[50px] rounded-[8px] font-[700px] bg-[#BACCE7] flex flex-col justify-end">Directorate of Technical Services</div>
+    <div className="grid grid-cols-5 gap-x-2 gap-y-2">
+    <DepartmentTile title="Information Technology Services" image={InfoTS} link=""/>
+    <DepartmentTile title="Satellite Navigation" image={SatNav} link=""/>
+    <DepartmentTile title="Network Operation Center" image={Network} link=""/>
+    <DepartmentTile title="Innovation" image={Innovation} link=""/>
+    <DepartmentTile title="Satellite Control and Operation" image={SatCtrl} link=""/>
+    <DepartmentTile title="Broadband" image={Broadband} link=""/>
+    <DepartmentTile title="Broadcast" image={Broadcast} link=""/>
+    <DepartmentTile title="Facility" image={Facilities} link=""/>
+    </div>
 
-      
-        <DepartmentTile title="Human Resources" image={HumanR} link=""/>
-        <DepartmentTile title="Budgeting and Planning" image={Budgeting} link=""/>
-        <DepartmentTile title="Information Technology Services" image={InfoTS} link=""/>
-        <DepartmentTile title="Administration" image={Administration} link=""/>
-        <DepartmentTile title="Broadcast" image={Broadcast} link=""/>
+    <div className="w-[1320px] h-[50px] rounded-[8px] font-[700px] bg-[#BACCE7] flex flex-col justify-end">Directorate of Marketing & Business Development</div>
+    <div className="grid grid-cols-5 gap-x-2 gap-y-2">
+    <DepartmentTile title="Marketing and Stakeholders Engagement" image={Marketing} link=""/>
+    <DepartmentTile title="Sales" image={Sales} link=""/>
+    <DepartmentTile title="Business Office" image={Business} link=""/>
+    </div>
     
-
-      
-        <DepartmentTile title="Sales" image={Sales} link=""/>
-        <DepartmentTile title="Business Office" image={Business} link=""/>
-        <DepartmentTile title="Satellite Navigation" image={SatNav} link=""/>
-        <DepartmentTile title="Broadband" image={Broadband} link=""/>
-        <DepartmentTile title="Satellite Control and Operation" image={SatCtrl} link=""/>
-      </div>
-
-    </div>
     </>
 
   );
