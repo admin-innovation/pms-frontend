7f6dca684622362b560632aa00b87c044f9504cf
official-MKV <vemmaks84@gmail.com>
2024-03-21 08:24:43 +0000

    Done with department Page

--- src/components/Department.jsx
+++ src/components/Department.jsx
@@ -5,11 +5,17 @@ import { Departments } from "../data/temp";
 import { MdOutlineMail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import ReactStars from "react-rating-stars-component";
+import { motion, AnimatePresence } from "framer-motion";
+import { IoCloseSharp } from "react-icons/io5";
 
 const Department = () => {
   const { department_id } = useParams();
   const data = Departments[department_id];
   const [staffView, setStaffView] = useState();
+  const closeStaffView = () => {
+    console.log("Clicked");
+    setStaffView(false);
+  };
 
   return (
     <div className="w-full h-full flex flex-col gap-[8px]">
@@ -20,7 +26,7 @@ const Department = () => {
             style={{ objectFit: "cover" }}
             src={`/${data.wall_paper}`}
           />
-          <span className="z-50 relative text-[40px] text-white font-[700]">
+          <span className="z-50 relative top-[50%] left-[50%] text-[40px] text-white font-[700]">
             {department_id}
           </span>
         </div>
@@ -56,97 +62,124 @@ const Department = () => {
         </div>
       </div>
       <div className="w-full flex gap-[10px]">
-        <div className="w-full flex-[1] bg-[white] rounded-[8px] px-[60px] flex flex-wrap gap-[10px] pt-[100px] ">
-          <span className="absolute">Other Staff</span>
+        <motion.div
+          transition={{ duration: 0.8, delay: 1 }}
+          initial={{ width: "100%" }}
+          animate={staffView ? { width: "70%" } : { width: "100%" }}
+          key="expanded"
+          className="relative w-full  bg-[white] rounded-[8px] px-[60px] flex flex-wrap  gap-y-[10px] gap-x-[10px] pt-[100px] "
+        >
+          <span className="absolute top-5 font-bold text-[18px]">
+            Other Staff
+          </span>
           {data.employees?.map((employee, key) => {
             return <EmployeeCard employee={employee} setStaff={setStaffView} />;
           })}
-        </div>
-        {staffView && (
-          <div className="bg-white h-[600px] w-[320px] flex flex-col items-center justify-center rounded-[8px] pb-[10px]  ">
-            <div className="w-full h-[50%] relative flex flex-col items-start">
-              <div className="w-full h-[50%] relative flex items-center justify-center gap-[10px]">
+        </motion.div>
+
+        <AnimatePresence>
+          {staffView && (
+            <motion.div
+              initial={{ opacity: 0, top: "200px" }}
+              animate={{ opacity: 1, top: "0px" }}
+              exit={{ opacity: 0, top: 200 }}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              className=" relative bg-white h-[700px] max-w-[30%] flex flex-col items-center justify-center rounded-[8px] pb-[10px]  "
+            >
+              <div className="w-full h-[50%] relative flex flex-col items-start">
                 <div
-                  className=" relative bg-cover w-[80px] h-[80px] rounded-full"
-                  style={{
-                    backgroundImage: `url('/${staffView.profile_pic}')`,
-                  }}
+                  className="z-[1000] w-[20px] h-[20px] absolute right-3 top-3"
+                  onClick={closeStaffView}
                 >
+                  <IoCloseSharp className="hover:text-[red] text-[18px] cursor-pointer" />
+                </div>
+                <div className="w-full h-[50%] relative flex items-center justify-center gap-[10px]">
                   <div
-                    className={`bottom-2 right-1 absolute w-[10px] h-[10px] bg-[#74B72E] border-[3px] border-solid border-[#BACCE714] rounded-full`}
-                  />
+                    className=" relative bg-cover w-[80px] h-[80px] rounded-full"
+                    style={{
+                      backgroundImage: `url('/${staffView.profile_pic}')`,
+                    }}
+                  >
+                    <div
+                      className={`bottom-2 right-1 absolute w-[10px] h-[10px] bg-[#74B72E] border-[3px] border-solid border-[#BACCE714] rounded-full`}
+                    />
+                  </div>
+                  <div className="flex flex-col justify-center">
+                    <span className="flex flex-row">
+                      <p className="text-[14px] font-[600]">
+                        Average task Rating:
+                      </p>
+                      <ReactStars
+                        count={5}
+                        // FIx these stars
+                        value={5}
+                        isHalf={true}
+                        edit={false}
+                        emptyIcon={
+                          <i className="far fa-star-o" aria-hidden="true"></i>
+                        }
+                        halfIcon={
+                          <i
+                            className="fa fa-star-half-o"
+                            aria-hidden="true"
+                          ></i>
+                        }
+                        fullIcon={<i className="fa fa-star"></i>}
+                        size={15}
+                        activeColor="#ffd700"
+                      />
+                    </span>
+                    <span className="flex">
+                      <p className="text-[14px] font-[600]">
+                        Average peer Rating:
+                      </p>
+                      <ReactStars
+                        count={5}
+                        value={5}
+                        isHalf={true}
+                        edit={false}
+                        emptyIcon={
+                          <i className="fa fa-star-o" aria-hidden="true"></i>
+                        }
+                        // onChange={ratingChanged}
+                        size={15}
+                        activeColor="#ffd700"
+                      />
+                    </span>
+                  </div>
                 </div>
-                <div className="flex flex-col justify-center">
-                  <span className="flex flex-row">
-                    <p className="text-[14px] font-[600]">
-                      Average task Rating:
-                    </p>
-                    <ReactStars
-                      count={5}
-                      // FIx these stars
-                      value={5}
-                      isHalf={true}
-                      edit={false}
-                      emptyIcon={
-                        <i className="far fa-star-o" aria-hidden="true"></i>
-                      }
-                      halfIcon={
-                        <i className="fa fa-star-half-o" aria-hidden="true"></i>
-                      }
-                      fullIcon={<i className="fa fa-star"></i>}
-                      size={15}
-                      activeColor="#ffd700"
-                    />
+                <div className="w-full flex flex-col px-[25px] gap-[10px]">
+                  <span className="flex flex-col font-bold text-[16px]">
+                    Name:
+                    <span className="text-[14px] font-[300]">
+                      {staffView.name}
+                    </span>
                   </span>
-                  <span className="flex">
-                    <p className="text-[14px] font-[600]">
-                      Average peer Rating:
-                    </p>
-                    <ReactStars
-                      count={5}
-                      value={5}
-                      isHalf={true}
-                      edit={false}
-                      emptyIcon={
-                        <i className="fa fa-star-o" aria-hidden="true"></i>
-                      }
-                      // onChange={ratingChanged}
-                      size={15}
-                      activeColor="#ffd700"
-                    />
+                  <span className="flex flex-col font-bold text-[16px]">
+                    Email Address:
+                    <span className="text-[14px] font-[300]">
+                      {staffView.email}
+                    </span>
+                  </span>
+                  <span className="flex flex-col font-bold text-[16px]">
+                    PhoneNumber:
+                    <span className="text-[14px] font-[300]">
+                      {staffView.phone}
+                    </span>
+                  </span>
+                  <span className="flex flex-col font-bold text-[16px]">
+                    Position:
+                    <span className="text-[14px] font-[300]">
+                      {staffView.position}
+                    </span>
                   </span>
                 </div>
               </div>
-              <div className="w-full flex flex-col px-[25px] gap-[10px]">
-                <span className="flex flex-col font-bold text-[16px]">
-                  Name:
-                  <span className="text-[14px] font-[300]">
-                    {staffView.name}
-                  </span>
-                </span>
-                <span className="flex flex-col font-bold text-[16px]">
-                  Email Address:
-                  <span className="text-[14px] font-[300]">
-                    {staffView.email}
-                  </span>
-                </span>
-                <span className="flex flex-col font-bold text-[16px]">
-                  PhoneNumber:
-                  <span className="text-[14px] font-[300]">
-                    {staffView.phone}
-                  </span>
-                </span>
-                <span className="flex flex-col font-bold text-[16px]">
-                  Position:
-                  <span className="text-[14px] font-[300]">
-                    {staffView.position}
-                  </span>
-                </span>
-              </div>
-            </div>
-            <TasksView />
-          </div>
-        )}
+
+              <TasksView />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
@@ -155,6 +188,9 @@ const Department = () => {
 const TasksView = ({ employee_id }) => {
   const [taskFilter, setTaskFilter] = useState("completed");
   // UseEffect to get tasks for employee with id_employee_id
+  {
+    /* Add loading State and empty state */
+  }
   const employee_task_data = [
     "Conduct a feasibility study for expanding Nigcomsat's satellite coverage to rural areas.",
     "Develop a strategic plan for enhancing Nigcomsat's satellite communication services.",
@@ -199,7 +235,7 @@ const TasksView = ({ employee_id }) => {
           </div>
         </div>
       </div>
-      <div className="relative h-full mb-[30px]  w-full overflow-y-scroll flex flex-col gap-[20px] ">
+      <div className="relative h-full mb-[30px] pt-[30px]  w-full overflow-y-scroll flex flex-col gap-[20px] ">
         {employee_task_data.map((item, key) => {
           return (
             <div className=" h-[60px] flex gap-[5px] w-full justify-center items-start">
@@ -226,7 +262,7 @@ const EmployeeCard = ({ employee, setStaff }) => {
   };
   return (
     <div
-      className="w-[150px] h-[150px] flex flex-col gap-[10px] items-center justify-center
+      className=" transition-all duration-300 ease-in-out w-[150px] h-[150px] flex flex-col gap-[10px] items-center justify-center
     rounded-[8px] bg-[#BACCE714]/10 hover:bg-[#BACCE714]/30 cursor-pointer"
       data-phone={employee.phone}
       data-email={employee.email}
