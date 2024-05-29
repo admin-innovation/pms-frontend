b04c579b2c8dc678b25a96bee22518745b44e727
official-MKV <vemmaks84@gmail.com>
2024-05-29 09:07:56 +0100

    added firbase.json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host:'test.me',
-    port:5173,
-    cors: true,
-
+  build: {
+    outDir: 'dist', // Specify the output directory
+    sourcemap: true, // Optional: for debugging purposes
   },
-})
+});
